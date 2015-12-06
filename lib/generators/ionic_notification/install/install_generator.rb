f28280a4710e93cbcef5c9964f7c7c0b7fe4f718
Francesco Renzi <rentziass@gmail.com>
2015-12-06 22:50:25 +0100

    Added description to generator

--- lib/generators/ionic_notification/install/install_generator.rb
+++ lib/generators/ionic_notification/install/install_generator.rb
@@ -6,6 +6,8 @@ module IonicNotification
 
       source_root File.expand_path('../templates', __FILE__)
 
+      desc "Generates IonicNotification initializer"
+
       def copy_config
         copy_file "ionic_notification.rb", "config/initializers/ionic_notification.rb"
       end
