3b9e53f549718d49b3067ed78634c1bbc7f8e6ba
Francesco Renzi <rentziass@gmail.com>
2015-12-08 15:01:30 +0100

    Conflict with rails logger

--- lib/ionic_notification/concerns/ionic_notificable.rb
+++ lib/ionic_notification/concerns/ionic_notificable.rb
@@ -7,23 +7,23 @@ module IonicNotification
         serialize :device_tokens, Array
 
         def notify(options = {})
-          return logger.missing_device_tokens unless respond_to?(:device_tokens)
-          return logger.no_device_tokens(self) unless device_tokens.count > 0
+          return new_logger.missing_device_tokens unless respond_to?(:device_tokens)
+          return new_logger.no_device_tokens(self) unless device_tokens.count > 0
           notification = IonicNotification::Notification.new(options.merge!(tokens: device_tokens))
           notification.send
         end
 
         def self.notify_all(options = {})
-          return logger.missing_device_tokens unless method_defined?(:device_tokens)
+          return new_logger.missing_device_tokens unless method_defined?(:device_tokens)
           tokens = all.map(&:device_tokens).flatten.uniq
-          return logger.no_device_tokens(self) unless tokens.count > 0
+          return new_logger.no_device_tokens(self) unless tokens.count > 0
           notification = IonicNotification::Notification.new(options.merge!(tokens: tokens))
           notification.send
         end
 
         private
 
-        def logger
+        def new_logger
           IonicNotification::Logger.new
         end
       end
