a7bee358b5f48d3bcbd9b004d025d6308703c2f6
Francesco Renzi <rentziass@gmail.com>
2015-12-08 14:54:27 +0100

    Logger instance

--- lib/ionic_notification/concerns/ionic_notificable.rb
+++ lib/ionic_notification/concerns/ionic_notificable.rb
@@ -7,18 +7,24 @@ module IonicNotification
         serialize :device_tokens, Array
 
         def notify(options = {})
-          return missing_device_tokens_logger unless respond_to?(:device_tokens)
-          return no_device_tokens_logger(self) unless device_tokens.count > 0
+          return logger.missing_device_tokens unless respond_to?(:device_tokens)
+          return logger.no_device_tokens(self) unless device_tokens.count > 0
           notification = IonicNotification::Notification.new(options.merge!(tokens: device_tokens))
           notification.send
         end
 
         def self.notify_all(options = {})
-          return missing_device_tokens_logger unless method_defined?(:device_tokens)
+          return logger.missing_device_tokens unless method_defined?(:device_tokens)
           tokens = all.map(&:device_tokens).flatten.uniq
-          return no_device_tokens_logger(self) unless tokens.count > 0
+          return logger.no_device_tokens(self) unless tokens.count > 0
           notification = IonicNotification::Notification.new(options.merge!(tokens: tokens))
           notification.send
+        end
+
+        private
+
+        def logger
+          IonicNotification::Logger.new
         end
       end
     end
