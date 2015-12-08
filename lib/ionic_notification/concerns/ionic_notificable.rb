33882eb99db3f757a63d2ad66b20930e612e0c68
Francesco Renzi <rentziass@gmail.com>
2015-12-08 11:50:03 +0100

    Added some loggers

--- lib/ionic_notification/concerns/ionic_notificable.rb
+++ lib/ionic_notification/concerns/ionic_notificable.rb
@@ -7,8 +7,24 @@ module IonicNotification
         serialize :device_tokens, Array
 
         def notify(options = {})
+          return missing_device_tokens_logger unless respond_to?(:device_tokens)
+          return no_device_tokens_logger unless device_tokens.count > 0
           notification = IonicNotification::Notification.new(options.merge!(tokens: device_tokens))
           notification.send
+        end
+
+        private
+
+        def no_device_tokens_logger
+          logger.debug "#{logger_label} No device tokens were found for #{self}, skipping."
+        end
+
+        def missing_device_tokens_logger
+          logger.debug "#{logger_label} This model does not respond to :device_tokens, did you run your migrations?"
+        end
+
+        def logger_label
+          "IonicNotification:"
         end
       end
     end
