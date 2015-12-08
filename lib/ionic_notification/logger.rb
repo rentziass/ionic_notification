f12b5d529f3818fdd058c45ba5949c1154422efd
Francesco Renzi <rentziass@gmail.com>
2015-12-08 13:25:26 +0100

    Made logger available to class

--- lib/ionic_notification/logger.rb
+++ lib/ionic_notification/logger.rb
@@ -1,16 +1,16 @@
 module IonicNotification
   module Logger
-    def no_device_tokens_logger(instance)
+    def self.no_device_tokens_logger(instance)
       return unless available?
       ionic_logger "#{logger_label} No device tokens were found for #{instance}, skipping."
     end
 
-    def missing_device_tokens_logger
+    def self.missing_device_tokens_logger
       return unless available?
       ionic_logger "#{logger_label} This model does not respond to :device_tokens, did you run your migrations?"
     end
 
-    def logger_label
+    def self.logger_label
       "IonicNotification:"
     end
 
